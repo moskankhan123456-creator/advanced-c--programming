99544778a90d810e75e46bf083e5bb976fc7a598
moskankhan123456-creator <moskankhan123456@gmail.com>
2026-09-12 23:07:21 +0500

    Create 08_null_pointer.cpp

---
+++ 04_pointers_and_memory/pointers/08_null_pointer.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int *ptr = nullptr;
+
+    if (ptr == nullptr)
+    {
+        cout << "Pointer is null.";
+    }
+
+    return 0;
+}
