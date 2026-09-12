764b68bfe3b85591086591a8f78bf7573bd6c25d
moskankhan123456-creator <moskankhan123456@gmail.com>
2026-09-12 15:19:08 +0500

    Add constants example

---
+++ variables_and_data_types/02_constants.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    const double PI = 3.14159265359;
+    const int MAX_STUDENTS = 50;
+
+    cout << "PI: " << PI << endl;
+    cout << "Maximum Students: " << MAX_STUDENTS << endl;
+
+    return 0;
+}
