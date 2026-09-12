eb9fbe3f30a0d5440c239bc19a6c310a96308a33
moskankhan123456-creator <moskankhan123456@gmail.com>
2026-09-12 15:31:35 +0500

    Add auto and type deduction example

---
+++ variables_and_data_types/06_auto_and_type_deduction.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    auto age = 18;
+    auto cgpa = 3.25;
+    auto grade = 'A';
+    auto name = string("Muskan");
+
+    cout << "Age: " << age << endl;
+    cout << "CGPA: " << cgpa << endl;
+    cout << "Grade: " << grade << endl;
+    cout << "Name: " << name << endl;
+
+    return 0;
+}
