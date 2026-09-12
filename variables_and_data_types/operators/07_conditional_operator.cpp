ed60bb5e46d668f99e079d833124e38a0360ccb5
moskankhan123456-creator <moskankhan123456@gmail.com>
2026-09-12 16:18:54 +0500

    Add conditional operator example

---
+++ variables_and_data_types/operators/07_conditional_operator.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 25;
+    int b = 40;
+
+    int maximum = (a > b) ? a : b;
+
+    cout << "Maximum value: " << maximum << endl;
+
+    return 0;
+}
