2c84cd80d7571d6922b52fff1209785464b95fd5
moskankhan123456-creator <moskankhan123456@gmail.com>
2026-09-12 22:48:28 +0500

    Create 07_matrix_transpose.cpp

---
+++ 03_arrays_and_strings/two_dimensional_arrays/07_matrix_transpose.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int matrix[2][3] = {
+        {1, 2, 3},
+        {4, 5, 6}
+    };
+
+    for (int j = 0; j < 3; j++)
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            cout << matrix[i][j] << " ";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
