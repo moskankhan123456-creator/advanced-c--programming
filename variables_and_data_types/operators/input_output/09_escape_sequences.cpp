e06da72a540a5f7590e6fc0aa1cb508d1a358e7d
moskankhan123456-creator <moskankhan123456@gmail.com>
2026-09-12 16:31:09 +0500

    Add escape sequences example

---
+++ variables_and_data_types/operators/input_output/09_escape_sequences.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "C++ Programming\n";
+    cout << "Artificial Intelligence\tStudent\n";
+    cout << "She said, \"C++ is powerful.\"\n";
+    cout << "Path: C:\\Projects\\CPP\n";
+
+    return 0;
+}
