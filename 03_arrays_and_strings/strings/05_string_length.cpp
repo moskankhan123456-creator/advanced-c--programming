e2c34948d43ba310feb824ab54e6162b055083e5
moskankhan123456-creator <moskankhan123456@gmail.com>
2026-09-12 22:54:29 +0500

    Create 05_string_length.cpp

---
+++ 03_arrays_and_strings/strings/05_string_length.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string text = "Artificial Intelligence";
+
+    cout << "Length = " << text.length();
+
+    return 0;
+}
