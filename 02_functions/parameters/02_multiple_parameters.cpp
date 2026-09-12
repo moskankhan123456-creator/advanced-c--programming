40ab6e3e75ae9921b59f2186938ca233b7d034ac
moskankhan123456-creator <moskankhan123456@gmail.com>
2026-09-12 18:32:36 +0500

    Create 02_multiple_parameters.cpp

---
+++ 02_functions/parameters/02_multiple_parameters.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void displayInfo(string name, int age)
+{
+    cout << "Name = " << name << endl;
+    cout << "Age = " << age;
+}
+
+int main()
+{
+    displayInfo("Dua", 18);
+
+    return 0;
+}
