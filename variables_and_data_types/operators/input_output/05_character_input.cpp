4942f3eb4c6ccf7e6d61ebfc2a92c43647764102
moskankhan123456-creator <moskankhan123456@gmail.com>
2026-09-12 16:28:45 +0500

    Add character input  example

---
+++ variables_and_data_types/operators/input_output/05_character_input.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char firstCharacter;
+    char secondCharacter;
+
+    cout << "Enter two characters: ";
+    cin >> firstCharacter >> secondCharacter;
+
+    cout << "First Character: " << firstCharacter << endl;
+    cout << "Second Character: " << secondCharacter << endl;
+
+    return 0;
+}
