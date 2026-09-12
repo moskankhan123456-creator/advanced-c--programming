2133c94ca9deb4bbe1a11892ee6240f581e70ea0
moskankhan123456-creator <moskankhan123456@gmail.com>
2026-09-12 18:24:18 +0500

    Create 04_function_call.cpp

---
+++ 02_functions/04_function_call.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void firstFunction()
+{
+    cout << "First function called." << endl;
+}
+
+void secondFunction()
+{
+    cout << "Second function called." << endl;
+}
+
+int main()
+{
+    firstFunction();
+    secondFunction();
+
+    return 0;
+}
