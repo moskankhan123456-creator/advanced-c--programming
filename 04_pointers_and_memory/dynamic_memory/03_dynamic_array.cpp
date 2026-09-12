01d66ae024e26a8d1cbded563c65ab2b3987a1fb
moskankhan123456-creator <moskankhan123456@gmail.com>
2026-09-12 23:18:27 +0500

    Create 03_dynamic_array.cpp

---
+++ 04_pointers_and_memory/dynamic_memory/03_dynamic_array.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int *numbers = new int[5];
+
+    for (int i = 0; i < 5; i++)
+    {
+        numbers[i] = (i + 1) * 10;
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout << numbers[i] << " ";
+    }
+
+    delete[] numbers;
+
+    return 0;
+}
