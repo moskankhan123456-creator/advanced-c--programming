de0b5c66ef895508b77a3f23134b5a23c07fbce3
moskankhan123456-creator <moskankhan123456@gmail.com>
2026-09-12 18:33:26 +0500

    Create 04_pass_by_reference.cpp

---
+++ 02_functions/parameters/04_pass_by_reference.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void changeValue(int &number)
+{
+    number = 100;
+}
+
+int main()
+{
+    int value = 50;
+
+    changeValue(value);
+
+    cout << "Value = " << value;
+
+    return 0;
+}
