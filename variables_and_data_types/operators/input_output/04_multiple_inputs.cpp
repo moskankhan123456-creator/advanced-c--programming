9d08c3c5dd2161260d3ce25324b2ace18fefd0e9
moskankhan123456-creator <moskankhan123456@gmail.com>
2026-09-12 16:28:02 +0500

    Add multiple inputs example

---
+++ variables_and_data_types/operators/input_output/04_multiple_inputs.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name;
+    int age;
+    double cgpa;
+
+    cout << "Enter name, age and CGPA: ";
+    cin >> name >> age >> cgpa;
+
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+    cout << "CGPA: " << cgpa << endl;
+
+    return 0;
+}
