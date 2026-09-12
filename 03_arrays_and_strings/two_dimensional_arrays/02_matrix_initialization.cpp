b45b05af9a90537104b3afe56a6cca6efafc3a79
moskankhan123456-creator <moskankhan123456@gmail.com>
2026-09-12 22:46:41 +0500

    Create 02_matrix_initialization.cpp

---
+++ 03_arrays_and_strings/two_dimensional_arrays/02_matrix_initialization.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int matrix[2][3] = {
+        {1, 2, 3},
+        {4, 5, 6}
+    };
+
+    cout << matrix[0][0] << endl;
+    cout << matrix[1][2];
+
+    return 0;
+}
