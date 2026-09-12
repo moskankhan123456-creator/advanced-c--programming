e410aa83477eeaec8db44b71ebb3d632003347fc
moskankhan123456-creator <moskankhan123456@gmail.com>
2026-09-12 22:54:10 +0500

    Create 04_string_concatenation.cpp

---
+++ 03_arrays_and_strings/strings/04_string_concatenation.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string firstName = "Muskan";
+    string lastName = "Khan";
+
+    string fullName = firstName + " " + lastName;
+
+    cout << fullName;
+
+    return 0;
+}
