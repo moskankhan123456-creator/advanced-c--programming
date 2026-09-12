6a98fef8ef465b2722417a05d7c01e5d857e9494
moskankhan123456-creator <moskankhan123456@gmail.com>
2026-09-12 22:54:50 +0500

    Create 06_string_comparison.cpp

---
+++ 03_arrays_and_strings/strings/06_string_comparison.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string first = "AI";
+    string second = "AI";
+
+    if (first == second)
+    {
+        cout << "Strings are equal.";
+    }
+    else
+    {
+        cout << "Strings are different.";
+    }
+
+    return 0;
+}
