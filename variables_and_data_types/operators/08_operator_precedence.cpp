e35e33f6315f1266826c1811ca3b202b33f39bb2
moskankhan123456-creator <moskankhan123456@gmail.com>
2026-09-12 16:19:31 +0500

    Add operator precedence example

---
+++ variables_and_data_types/operators/08_operator_precedence.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int result1 = 10 + 5 * 2;
+    int result2 = (10 + 5) * 2;
+    int result3 = 20 / 4 + 3 * 2;
+
+    cout << "10 + 5 * 2 = " << result1 << endl;
+    cout << "(10 + 5) * 2 = " << result2 << endl;
+    cout << "20 / 4 + 3 * 2 = " << result3 << endl;
+
+    return 0;
+}
