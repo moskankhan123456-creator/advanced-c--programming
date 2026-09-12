937f08ca1f5f954e5bc83ad0a3c1f4252d27537f
moskankhan123456-creator <moskankhan123456@gmail.com>
2026-09-12 22:53:45 +0500

    Create 03_string_indexing.cpp

---
+++ 03_arrays_and_strings/strings/03_string_indexing.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string word = "Computer";
+
+    cout << word[0] << endl;
+    cout << word[3];
+
+    return 0;
+}
